0f7e71697cf664c37e1191c1ce5e67cb2bd4c9a8
Bobi2000 <bobko_2000@abv.bg>
2020-02-13 05:32:46 +0200

    Adding index and bootstrap.
    Signed-off-by: Bobi2000 <bobko_2000@abv.bg>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { AboutComponent } from './about/about.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent
-=======
 
-@NgModule({
-  declarations: [
-    AppComponent
->>>>>>> 3a0a23765287ccb1f5fab28aa50d9d3669ab036b
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
